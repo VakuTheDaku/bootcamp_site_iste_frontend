6fe05a52fd6d12a8d4d472297488ec63612f4186
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2021-12-21 04:32:29 +0530

    responsive_aakkal

---
+++ anime.js
@@ -0,0 +1,115 @@
+var a1=document.querySelector("#wee1")
+var b=document.querySelectorAll(".card")
+var c=document.querySelectorAll(".card-text")
+
+a1.addEventListener("click",function(){
+    
+    if(c[0].textContent=="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"){
+       console.log("working")
+       c[0].innerHTML= "<b> INSTALLATION AND SETTING UP</b>"
+       b[0].classList.toggle("changecol")
+       a1.classList.toggle("rotate")
+    }
+    else{
+        
+    
+b[0].classList.toggle("changecol")
+a1.classList.toggle("rotate")
+c[0].textContent="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"
+    }
+})
+var a2=document.querySelector("#wee2")
+
+
+a2.addEventListener("click",function(){
+    
+    if(c[1].textContent=="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"){
+       console.log("working")
+       c[1].innerHTML= "<b> INTRODUCTION TO WEB DEVELOPMENT</b>"
+       b[1].classList.toggle("changecol")
+       a2.classList.toggle("rotate")
+    }
+    else{
+        
+    
+b[1].classList.toggle("changecol")
+a2.classList.toggle("rotate")
+c[1].textContent="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"
+    }
+})
+var a3=document.querySelector("#wee3")
+
+
+a3.addEventListener("click",function(){
+    
+    if(c[2].textContent=="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"){
+       console.log("working")
+       c[2].innerHTML= "<b> INTRODUCTION TO WEB DEVELOPMENT</b>"
+       b[2].classList.toggle("changecol")
+       a3.classList.toggle("rotate")
+    }
+    else{
+        
+    
+b[2].classList.toggle("changecol")
+a3.classList.toggle("rotate")
+c[2].textContent="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"
+    }
+})
+var a4=document.querySelector("#wee4")
+
+
+a4.addEventListener("click",function(){
+    
+    if(c[3].textContent=="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"){
+       console.log("working")
+       c[3].innerHTML= "<b> INTRODUCTION TO WEB DEVELOPMENT</b>"
+       b[3].classList.toggle("changecol")
+       a4.classList.toggle("rotate")
+    }
+    else{
+        
+    
+b[3].classList.toggle("changecol")
+a4.classList.toggle("rotate")
+c[3].textContent="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"
+    }
+})
+var a5=document.querySelector("#wee5")
+
+
+a5.addEventListener("click",function(){
+    
+    if(c[4].textContent=="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"){
+       console.log("working")
+       c[4].innerHTML= "<b> INTRODUCTION TO WEB DEVELOPMENT</b>"
+       b[4].classList.toggle("changecol")
+       a5.classList.toggle("rotate")
+    }
+    else{
+        
+    
+b[4].classList.toggle("changecol")
+a5.classList.toggle("rotate")
+c[4].textContent="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"
+    }
+})
+var a6=document.querySelector("#wee6")
+
+
+a6.addEventListener("click",function(){
+    
+    if(c[5].textContent=="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"){
+       console.log("working")
+       c[5].innerHTML= "<b> INTRODUCTION TO WEB DEVELOPMENT</b>"
+       b[5].classList.toggle("changecol")
+       a6.classList.toggle("rotate")
+    }
+    else{
+        
+    
+b[5].classList.toggle("changecol")
+a6.classList.toggle("rotate")
+c[5].textContent="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dicta corporis itaque rerum officia quidem sapiente. Optio quia error, nemo incidunt recusandae inventore veritatis at. Itaque quia totam facilis reiciendis?"
+    }
+})
