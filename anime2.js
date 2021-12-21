370021b835e3bc05982f5d0f2a8bd392fe396119
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2021-12-21 23:08:56 +0530

    changes

---
+++ anime2.js
@@ -0,0 +1,95 @@
+var f=document.querySelectorAll(".card .card-text .row .col-lg-2 .bi")
+var h=document.querySelectorAll(".card .card-text")
+var e=document.querySelectorAll(".card .card-text .row .col-lg-6")
+
+var g=document.querySelectorAll(".card")
+function after0(){
+    if(e[0].innerHTML=="<b>Day 1 - INSTALLATION AND SETTING UP</b>"){
+    f[0].classList.remove("bi-plus-lg")
+    f[0].classList.add("bi-dash-lg")
+    g[0].classList.add("coloor")
+    e[0].innerHTML="hjgdfdshfgdshgchgchjdfsdgfdgfdfdfsdfsADSADFSDSFDSDFSDFSDFDXFDFGDFHFHGFH"
+    }
+    else{
+        f[0].classList.remove("bi-dash-lg")
+    f[0].classList.add("bi-plus-lg")
+    g[0].classList.remove("coloor")
+    e[0].innerHTML="<b>Day 1 - INSTALLATION AND SETTING UP</b>"
+    }
+    
+}
+function after1(){
+    if(e[1].innerHTML=="<b>Day 1 - INSTALLATION AND SETTING UP</b>"){
+    f[1].classList.remove("bi-plus-lg")
+    f[1].classList.add("bi-dash-lg")
+    g[1].classList.add("coloor")
+    e[1].innerHTML="hjgdfdshfgdshgchgchjdfsdgfdgfdfdfsdfsADSADFSDSFDSDFSDFSDFDXFDFGDFHFHGFH"
+    }
+    else{
+        f[1].classList.remove("bi-dash-lg")
+    f[1].classList.add("bi-plus-lg")
+    g[1].classList.remove("coloor")
+    e[1].innerHTML="<b>Day 1 - INSTALLATION AND SETTING UP</b>"
+    }
+    
+}
+function after2(){
+    if(e[2].innerHTML=="<b>Day 1 - INSTALLATION AND SETTING UP</b>"){
+    f[2].classList.remove("bi-plus-lg")
+    f[2].classList.add("bi-dash-lg")
+    g[2].classList.add("coloor")
+    e[2].innerHTML="hjgdfdshfgdshgchgchjdfsdgfdgfdfdfsdfsADSADFSDSFDSDFSDFSDFDXFDFGDFHFHGFH"
+    }
+    else{
+        f[2].classList.remove("bi-dash-lg")
+    f[2].classList.add("bi-plus-lg")
+    g[2].classList.remove("coloor")
+    e[2].innerHTML="<b>Day 1 - INSTALLATION AND SETTING UP</b>"
+    }
+    
+}
+function after3(){
+    if(e[3].innerHTML=="<b>Day 1 - INSTALLATION AND SETTING UP</b>"){
+    f[3].classList.remove("bi-plus-lg")
+    f[3].classList.add("bi-dash-lg")
+    g[3].classList.add("coloor")
+    e[3].innerHTML="hjgdfdshfgdshgchgchjdfsdgfdgfdfdfsdfsADSADFSDSFDSDFSDFSDFDXFDFGDFHFHGFH"
+    }
+    else{
+        f[3].classList.remove("bi-dash-lg")
+    f[3].classList.add("bi-plus-lg")
+    g[3].classList.remove("coloor")
+    e[3].innerHTML="<b>Day 1 - INSTALLATION AND SETTING UP</b>"
+    }
+    
+}
+function after4(){
+    if(e[4].innerHTML=="<b>Day 1 - INSTALLATION AND SETTING UP</b>"){
+    f[4].classList.remove("bi-plus-lg")
+    f[4].classList.add("bi-dash-lg")
+    g[4].classList.add("coloor")
+    e[4].innerHTML="hjgdfdshfgdshgchgchjdfsdgfdgfdfdfsdfsADSADFSDSFDSDFSDFSDFDXFDFGDFHFHGFH"
+    }
+    else{
+        f[4].classList.remove("bi-dash-lg")
+    f[4].classList.add("bi-plus-lg")
+    g[4].classList.remove("coloor")
+    e[4].innerHTML="<b>Day 1 - INSTALLATION AND SETTING UP</b>"
+    }
+    
+}
+function after5(){
+    if(e[5].innerHTML=="<b>Day 1 - INSTALLATION AND SETTING UP</b>"){
+    f[5].classList.remove("bi-plus-lg")
+    f[5].classList.add("bi-dash-lg")
+    g[5].classList.add("coloor")
+    e[5].innerHTML="hjgdfdshfgdshgchgchjdfsdgfdgfdfdfsdfsADSADFSDSFDSDFSDFSDFDXFDFGDFHFHGFH"
+    }
+    else{
+        f[5].classList.remove("bi-dash-lg")
+    f[5].classList.add("bi-plus-lg")
+    g[5].classList.remove("coloor")
+    e[5].innerHTML="<b>Day 1 - INSTALLATION AND SETTING UP</b>"
+    }
+    
+}
